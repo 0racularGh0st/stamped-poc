ef53d7b397ad30762ae3cb2b521b35d3e25c0bbd
Nigel David <nigel.david@founderandlightning.com>
2023-10-17 12:53:41 +0530

    update

--- scripts/stamped.tsx
+++ scripts/stamped.tsx
@@ -7,16 +7,14 @@ declare global {
 }
 let StampedFn:any;
 const loadStamped = () => {
-    console.log("here1");
     StampedFn = window.StampedFn;
         StampedFn.init({ apiKey: '2809bb1f-a1ca-4c22-bdbe-5346a0a59b49', storeUrl: 'stamped-poc.vercel.app', sId: '303927' });
         StampedFn.on('stamped:init:starting', function(){
-            console.log("here2");
             StampedFn.initRewards({
-                customerId: '123', // your own internal numeric ID of the customer
-                customerEmail: 'nigelcustomer2@yopmail.com',
+                customerId: '916da630-459b-4c6d-af1f-600a770a27a2',
+                customerEmail: 'nigel2@yopmail.com',
                 customerFirstName: 'nigel',
-                customerLastName: 'tariang',
+                customerLastName: 'david',
             });
         });
        }
